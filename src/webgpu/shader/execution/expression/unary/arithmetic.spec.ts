62d67437be8dd2722ee1feae5b4a2f0235480fa8
Antonio Maiorano <amaiorano@google.com>
2023-01-13 13:54:51 -0500

    Add unary i32 execution tests

--- src/webgpu/shader/execution/expression/unary/arithmetic.spec.ts
+++ src/webgpu/shader/execution/expression/unary/arithmetic.spec.ts
@@ -4,9 +4,9 @@ Execution Tests for the f32 arithmetic unary expression operations
 
 import { makeTestGroup } from '../../../../../common/framework/test_group.js';
 import { GPUTest } from '../../../../gpu_test.js';
-import { TypeF32 } from '../../../../util/conversion.js';
+import { i32, TypeF32, TypeI32 } from '../../../../util/conversion.js';
 import { negationInterval } from '../../../../util/f32_interval.js';
-import { fullF32Range } from '../../../../util/math.js';
+import { fullF32Range, fullI32Range } from '../../../../util/math.js';
 import { makeCaseCache } from '../case_cache.js';
 import { allInputSources, generateUnaryToF32IntervalCases, run } from '../expression.js';
 
@@ -14,17 +14,22 @@ import { unary } from './unary.js';
 
 export const g = makeTestGroup(GPUTest);
 
-export const d = makeCaseCache('unary/f32_arithmetic', {
-  negation: () => {
+export const d = makeCaseCache('unary/arithmetic', {
+  negation_f32: () => {
     return generateUnaryToF32IntervalCases(
       fullF32Range({ neg_norm: 250, neg_sub: 20, pos_sub: 20, pos_norm: 250 }),
       'unfiltered',
       negationInterval
     );
   },
+  negation_i32: () => {
+    return fullI32Range().map(e => {
+      return { input: i32(e), expected: i32(-e) };
+    });
+  },
 });
 
-g.test('negation')
+g.test('negation_f32')
   .specURL('https://www.w3.org/TR/WGSL/#floating-point-evaluation')
   .desc(
     `
@@ -36,6 +41,21 @@ Accuracy: Correctly rounded
     u.combine('inputSource', allInputSources).combine('vectorize', [undefined, 2, 3, 4] as const)
   )
   .fn(async t => {
-    const cases = await d.get('negation');
+    const cases = await d.get('negation_f32');
     await run(t, unary('-'), [TypeF32], TypeF32, t.params, cases);
+  });
+
+g.test('negation_i32')
+  .specURL('https://www.w3.org/TR/WGSL/#floating-point-evaluation')
+  .desc(
+    `
+Expression: -x
+`
+  )
+  .params(u =>
+    u.combine('inputSource', allInputSources).combine('vectorize', [undefined, 2, 3, 4] as const)
+  )
+  .fn(async t => {
+    const cases = await d.get('negation_i32');
+    await run(t, unary('-'), [TypeI32], TypeI32, t.params, cases);
   });
